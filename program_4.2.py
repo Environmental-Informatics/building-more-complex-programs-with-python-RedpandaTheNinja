05dcd959b4a033fe2210a39858c69b319c620001
Kevin Lee <lee423@purdue.edu>
2020-01-31 13:41:56 -0500

    2nd 3rd flower

--- program_4.2.py
+++ program_4.2.py
@@ -1,63 +1,49 @@
 import turtle
 import math
+
 bob = turtle.Turtle()
-bob.speed('fastest')
-# print(bob)
-# bob.fd(100)
-# bob.lt(90)
-# bob.fd(100)
-#
+bob.speed( 'fastest' )
 
-# def circle(t, r):
-#     circumference = 2 * math.pi *r
-#     n = 50
-#     length= circumference / n
-#     polygon(t, n, length)
-#     # arc(t, r, 360)
-def polygon(t,n, length):
-    angle = 360 /n
-    for i in range(n):
-        t.fd(length)
-        t.lt(angle)
 
-def arc(t,r,angle):
-    arc_length = 2 * math.pi * r * angle/360
-    n = int(arc_length/3)+1
+# polygon example from the book
+def polygon(t , n , length):
+    angle = 360 / n
+    for i in range( n ):
+        t.fd( length )
+        t.lt( angle )
+
+
+# arc example from the book
+def arc(t , r , angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int( arc_length / 3 ) + 1
     step_length = arc_length / n
     step_angle = angle / n
 
-    for i in range(n):
-        t.fd(step_length)
-        t.lt(step_angle)
+    for i in range( n ):
+        t.fd( step_length )
+        t.lt( step_angle )
 
-def circle(t,r):
-    arc(t,r,360)
 
-# polygon(bob, 7,70)
+# petal function to draw the petal
+def petal(t , r , angle):
+    for i in range( 2 ):
+        arc( t , r , angle )
+        t.lt( 180 - angle )
 
-def polyline(t , n , length, angle):
-    "draws n line seg with the given length and angle. t is a turtle"
-    for i in range (n):
-        t.fd(length)
-        t.lt(angle)
-def polygon(t,n,length):
-    angle = 360.0/n
-    polyline(t,n,length,angle)
 
-def petal( t, r, angle):
-    for i in range(2):
-        arc(t,r,angle)
-        t.lt(180-angle)
-def flower (t,n,r,angle,p):
+# flower function with n number of leaf
+def flower(t , n , r , angle , p):
     "draw flower with n petal"
-    for i in range(n):
-        petal(t,r,angle)
-        t.lt(p/n)
+    for i in range( n ):
+        petal( t , r , angle )
+        t.lt( p / n )
 
-#create 7 leaf flower
+
+# create 7 leaf flower
 # flower(bob, 7, 50, 60, 360)
-#overlapping leaf with more leaf , changed pedal num and angle compared to before
+# overlapping leaf with more leaf , changed pedal num and angle compared to before
 # flower(bob, 10, 50, 100, 360)
-#added more leaf and adjust angle to create thinner appearance
-flower(bob, 20, 90, 20, 360)
+# added more leaf and adjust angle to create thinner appearance
+flower( bob , 20 , 90 , 20 , 360 )
 turtle.mainloop()

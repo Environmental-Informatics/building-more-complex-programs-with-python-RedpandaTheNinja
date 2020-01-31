2ef5aa057627db89b5ece08540644fdc3ead11c4
Kevin Lee <lee423@purdue.edu>
2020-01-31 14:17:20 -0500

    2nd 3rd flower

--- program_5.2.py
+++ program_5.2.py
@@ -8,11 +8,14 @@ b = input(prompt)
 prompt = 'what is c?\n'
 c = input(prompt)
 prompt = 'what is n?\n'
+#converts all the inputs into int to process the calculation
 n = input(prompt)
 n = int(n)
 a = int(a)
 b = int(b)
 c = int(c)
+
+#instead of nested if, just add it as conditional statement
 if n > 2  and a^n + b^n == c^n:
     print("Holy smokes, Fermat was wrong!")
 else:

5e95298aa2b7cc81271227baa2701fd5cf88d30f
Kevin Lee <lee423@purdue.edu>
2020-01-31 23:58:43 -0500

    GCD implement
    7.1 completed

--- program_7.1.py
+++ program_7.1.py
@@ -1,13 +1,12 @@
 # Kevin Lee
 # assign 2
 
-# a =4
-# x = 3
 import math
 def mysqrt(a):
+    #creadting arbitrary num it cannot be 1 less, then it explode
     x = a-0.1
     while True:
-        # print( x )
+        # same code as the book
         y = (x + a / x) / 2
         if y == x:
             break
@@ -18,12 +17,13 @@ def test_square_root():
     a = 1
     print('\na      mysqrt(a)     math.sqrt(a)     diff')
     print('-----------------------------------------------------')
+    #make a loop to 1 to 9
     while a in range(10):
         sec_col = mysqrt(a)
         third_col = math.sqrt(a)
         diff = abs(third_col - sec_col)
+        #display proper floating points
         print('{0:0.1f}   {1:.11f}      {2:.11f}    {3}'.format(a,sec_col,third_col,diff))
-        # print('\n')
         a+=1
 
 test_square_root()
